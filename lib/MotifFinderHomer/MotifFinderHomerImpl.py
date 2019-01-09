531c86c7016a83ceece32fa1b27fdf13a32f231c
arwyer <awyer@vols.utk.edu>
2019-01-09 13:36:25 -0500

    turn off dust for narratorial

--- lib/MotifFinderHomer/MotifFinderHomerImpl.py
+++ lib/MotifFinderHomer/MotifFinderHomerImpl.py
@@ -276,10 +276,13 @@ class MotifFinderHomer:
         SSret =  SSU.buildFromFeatureSet(BuildParams)
         SSref = SSret['SequenceSet_ref']
         fastapath = '/kb/module/work/tmp/tmpSeqSet.fa'
-        FastaParams = {'workspace_name' : params['workspace_name'] , 'SequenceSetRef' : SSref , 'fasta_outpath' : fastapath}
-        output = self.BuildFastaFromSequenceSet(ctx,FastaParams)
         newfastapath = '/kb/module/work/tmp/SeqSet.fa'
-        RemoveRepeats(fastapath,newfastapath)
+        fastapath = newfastapath
+        FastaParams = {'workspace_name' : params['workspace_name'] , 'SequenceSetRef' : SSref , 'fasta_outpath' : fastapath}
+
+        output = self.BuildFastaFromSequenceSet(ctx,FastaParams)
+
+        #RemoveRepeats(fastapath,newfastapath)
         findmotifsparams= {'workspace_name' : params['workspace_name'],'fastapath':fastapath,'motif_min_length':params['motif_min_length'],'motif_max_length':params['motif_max_length'],'SS_ref':SSref,'obj_name' :params['obj_name']}
 
         output = self.find_motifs(ctx,findmotifsparams)[0]
